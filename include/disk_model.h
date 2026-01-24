e1b2e36669970520090ccd6234e42d34044baa2c
tnehezd <tarczaynehez.dora@csfk.org>
2026-01-25 00:57:27 +0100

    (refractor): update Hungarian comments in disk_model.c/.h

--- include/disk_model.h
+++ include/disk_model.h
@@ -4,26 +4,16 @@
 #define DISK_MODEL_H
 
 
-// Azért include-olom a config.h-t, mert a readDiskParameters globális változókat használ
-// (pl. SUN2GR, AU2CM), és a Parabola, createRadialGrid, createInitialGasSurfaceDensity is NGRID, RMIN, DD, SIGMA0, SIGMAP_EXP-et.
-// Bár ez nem szigorúan szükséges, mert a disk_model.c direktben include-olja a config.h-t,
-// jó gyakorlat, ha egy header is jelzi a függőségeit, ha a benne lévő deklarációk függenek tőlük.
+
 #include "config.h"
-#include "simulation_types.h" // Feltételezve, hogy itt található a disk_t definíciója
+#include "simulation_types.h" 
 
-// Funkciódeklarációk a disk_model.c-ből
 void readDiskParameters(disk_t *disk_params);
-
 void createRadialGrid(disk_t *disk_params);
-
 void createInitialGasSurfaceDensity(disk_t *disk_params);
-
 void createInitialGasPressure(disk_t *disk_params);
-
 void createInitialGasPressureGradient(disk_t *disk_params);
-
 void createInitialGasVelocity(disk_t *disk_params);
-
 void calculateDustSurfaceDensity(double radin[][2], double *massin, double out[][3], int n, const disk_t *disk_params);
 
 #endif // DISK_MODEL_H
