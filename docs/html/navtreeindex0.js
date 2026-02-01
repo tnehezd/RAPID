f3b3313a4c01948f55ec5b0a241e5907ac594810
tnehezd <tarczaynehez.dora@csfk.org>
2026-02-02 00:30:21 +0100

    (docs): update documentation

---
+++ docs/html/navtreeindex0.js
@@ -0,0 +1,6 @@
+var NAVTREEINDEX0 =
+{
+"index.html":[],
+"md__coding_standard.html":[0],
+"pages.html":[]
+};
---
+++ docs/html/search/all_1.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['naming_20conventions_0',['Naming Conventions',['../md__coding_standard.html',1,'']]]
+];
